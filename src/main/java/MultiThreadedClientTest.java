6745ba11c44f815d3127717eb082692bba08a663
Kenneth Jung <kmj@google.com>
2019-06-12 10:09:53 -0700

    Update for v1beta1.

--- src/main/java/MultiThreadedClientTest.java
+++ src/main/java/MultiThreadedClientTest.java
@@ -1,20 +1,23 @@
-import com.google.api.gax.rpc.ServerStream;
-import com.google.cloud.bigquery.storage.v1alpha1.BigQueryStorageClient;
-import com.google.cloud.bigquery.storage.v1alpha1.BigQueryStorageSettings;
-import com.google.cloud.bigquery.storage.v1alpha1.Storage.ReadRowsRequest;
-import com.google.cloud.bigquery.storage.v1alpha1.Storage.ReadRowsResponse;
-import com.google.cloud.bigquery.storage.v1alpha1.Storage.ReadSession;
-import com.google.cloud.bigquery.storage.v1alpha1.Storage.Stream;
-import com.google.cloud.bigquery.storage.v1alpha1.Storage.StreamPosition;
-import com.google.cloud.bigquery.v3.TableReferenceProto.TableReference;
+import com.google.cloud.bigquery.storage.v1beta1.BigQueryStorageClient;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.CreateReadSessionRequest;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.DataFormat;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadRowsRequest;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadRowsResponse;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.ReadSession;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.Stream;
+import com.google.cloud.bigquery.storage.v1beta1.Storage.StreamPosition;
+import com.google.cloud.bigquery.storage.v1beta1.TableReferenceProto.TableReference;
 import com.google.common.base.Stopwatch;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class MultiThreadedClientTest {
+
+  private static final Integer DEFAULT_NUM_THREADS = 4;
 
   private static final TableReference TABLE_REFERENCE = TableReference.newBuilder()
       .setProjectId("bigquery-public-data")
@@ -22,81 +25,102 @@ public class MultiThreadedClientTest {
       .setTableId("wikipedia")
       .build();
 
-  private static final int NUM_THREADS = 4;
+  private static class ReaderThread extends Thread {
 
-  private static void startReader(String endpoint, int threadId, Stream stream) throws Exception {
-
-    StreamPosition streamPosition = StreamPosition.newBuilder()
-        .setStream(stream)
-        .setOffset(0)
-        .build();
-
-    ReadRowsRequest request = ReadRowsRequest.newBuilder()
-        .setReadPosition(streamPosition)
-        .build();
+    final Stream stream;
 
     long numResponses = 0;
     long numRows = 0;
     long numTotalBytes = 0;
     long lastReportTimeNanos = 0;
 
-    try (BigQueryStorageClient client = getClient(endpoint)) {
-      Stopwatch stopwatch = Stopwatch.createStarted();
-      ServerStream<ReadRowsResponse> responseStream = client.readRowsCallable().call(request);
-      for (ReadRowsResponse response : responseStream) {
-        numResponses++;
-        numRows += response.getRowsCount();
-        numTotalBytes += response.getSerializedSize();
+    public ReaderThread(Stream stream) {
+      this.stream = stream;
+    }
 
-        long elapsedTimeNanos = stopwatch.elapsed(TimeUnit.NANOSECONDS);
-        if (elapsedTimeNanos - lastReportTimeNanos > TimeUnit.SECONDS.toNanos(10)) {
-          System.out.println(String.format(
-              "Thread %d received %d responses (%d rows) in 10s (%f MB/s)",
-              threadId, numResponses, numRows, (double) numTotalBytes / (1024 * 1024 * 10)));
-          numResponses = numRows = numTotalBytes = 0;
-          lastReportTimeNanos = elapsedTimeNanos;
-        }
+    public void run() {
+      try {
+        readRows();
+      } catch (Exception e) {
+        System.err.println(String.format("Caught %s while calling ReadRows; exiting", e));
       }
     }
 
-    System.out.println("Thread " + threadId + " done");
-  }
+    private void readRows() throws Exception {
 
-  private static BigQueryStorageClient getClient(String endpoint) throws IOException {
-    BigQueryStorageSettings settings =
-        BigQueryStorageSettings.newBuilder()
-            .setEndpoint(endpoint)
-            .build();
-    return BigQueryStorageClient.create(settings);
+      ReadRowsRequest request =
+          ReadRowsRequest.newBuilder()
+              .setReadPosition(StreamPosition.newBuilder().setStream(stream).setOffset(0))
+              .build();
+
+      try (BigQueryStorageClient client = BigQueryStorageClient.create()) {
+        Stopwatch stopwatch = Stopwatch.createStarted();
+        for (ReadRowsResponse response : client.readRowsCallable().call(request)) {
+          numResponses++;
+          numRows += response.getAvroRows().getRowCount();
+          numTotalBytes += response.getSerializedSize();
+          printPeriodicUpdate(stopwatch.elapsed(TimeUnit.NANOSECONDS));
+
+          // This is just a simple end-to-end throughput test, so we don't decode the Avro record
+          // block here. Note this may well have an impact on throughput in a normal use case!
+        }
+
+        stopwatch.stop();
+        printPeriodicUpdate(stopwatch.elapsed(TimeUnit.NANOSECONDS));
+        System.out.println("Finished reading from stream " + stream.getName());
+      }
+    }
+
+    private void printPeriodicUpdate(long elapsedTimeNanos) {
+      if (elapsedTimeNanos - lastReportTimeNanos >= TimeUnit.SECONDS.toNanos(10)) {
+        System.out.println(String.format(
+            "Received %d responses (%d rows) from stream %s in 10s (%f MB/s)",
+            numResponses, numRows, stream.getName(), (double) numTotalBytes / (1024 * 1024 * 10)));
+
+        numResponses = 0;
+        numRows = 0;
+        numTotalBytes = 0;
+        lastReportTimeNanos = elapsedTimeNanos;
+      }
+    }
   }
 
   public static void main(String[] args) throws Exception {
-    String endpoint = args[0];
-
-    System.out.println("Creating a new client with endpoint " + endpoint);
-
-    ReadSession readSession;
-    try (BigQueryStorageClient client = getClient(endpoint)) {
-      readSession = client.createReadSession(TABLE_REFERENCE, NUM_THREADS);
+    if (args.length < 1) {
+      throw new IllegalArgumentException("Parent project name is required");
     }
 
-    System.out.println("Created session with ID " + readSession.getName());
+    String parent = String.format("projects/%s", args[0]);
+    int numThreads = (args.length > 1) ? Integer.parseInt(args[1]) : DEFAULT_NUM_THREADS;
 
-    int numStreams = readSession.getStreamsCount();
-    ExecutorService executorService = Executors.newFixedThreadPool(numStreams);
-    for (int i = 0; i < numStreams; i++) {
-      final int thread_id = i;
-      executorService.submit(() -> {
-        try {
-          startReader(endpoint, thread_id, readSession.getStreams(thread_id));
-        } catch (Exception e) {
-          throw new RuntimeException(e);
-        }
-      });
+    CreateReadSessionRequest request =
+        CreateReadSessionRequest.newBuilder()
+            .setTableReference(TABLE_REFERENCE)
+            .setFormat(DataFormat.AVRO)
+            .setParent(parent)
+            .setRequestedStreams(numThreads)
+            .build();
+
+    List<ReaderThread> readerThreads = new ArrayList<>();
+
+    try (BigQueryStorageClient client = BigQueryStorageClient.create()) {
+      ReadSession readSession = client.createReadSession(request);
+      System.out.println(String.format("Created read session with ID %s", readSession.getName()));
+      for (Stream stream : readSession.getStreamsList()) {
+        System.out.println(
+            String.format("Creating a new reader thread for stream %s", stream.getName()));
+        readerThreads.add(new ReaderThread(stream));
+      }
     }
 
-    executorService.shutdown();
-    executorService.awaitTermination(30, TimeUnit.MINUTES);
-    System.out.println("Work items completed; exiting");
+    for (ReaderThread readerThread : readerThreads) {
+      readerThread.start();
+    }
+
+    for (ReaderThread readerThread : readerThreads) {
+      readerThread.join();
+    }
+
+    System.out.println("All reader threads finished; exiting");
   }
 }
